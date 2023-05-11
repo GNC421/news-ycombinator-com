d3ff8e584d3a4dc690442eeaacf41f5b2ca439b5
GNC421 <nunezcano.guillermo@gmail.com>
2023-05-11 13:26:12 +0200

    Exercise 2 finished

--- src/exercise2.py
+++ src/exercise2.py
@@ -12,9 +12,17 @@ def filterByTitle(array):
             result.append(index)
     return result
 
+#definition of sort's criteria
+def MyCriteria(e):
+    return e['score']
+
 def main():
     array = get30Entries()
     filtered = filterByTitle(array)
+    #order by number of comments
+    filtered.sort(key=MyCriteria)
+    #Here we have the first exercise's result
+    print(filtered)
 
 if __name__ == '__main__':
     main()
